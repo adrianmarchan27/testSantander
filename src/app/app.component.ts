2a6e9cb358e09a12656e0a9683b035174d6e818f
Adrian_Marchan <adrian@almacenguru.com>
2022-10-12 17:15:49 -0300

    finish task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IPokemon } from './interfaces/IPokemon.interface';
+import { SantanderService } from './services/santander.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testSantander';
+  listPokemon: IPokemon[] = [];
+
+  constructor(private santanderService: SantanderService) {}
+
+  ngOnInit() {
+    this.getPokemonsData();
+  }
+
+
+  getPokemonsData() {
+    this.santanderService.getPokemon()
+    .subscribe((results : IPokemon[]) => {
+      let listIds: string[] = results.map((result: any) => (result.url as string).substring(0,(result.url as string).length - 1).split('/').pop()!);
+      listIds.forEach((id: string) => this.santanderService.getPokemonById(id).subscribe(
+        (pokemon: IPokemon) => this.listPokemon.push(pokemon) 
+      )
+    );
+    });
+  } 
+
 }
---
+++ src/app/services/santander.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http' 
+import { IPokemon } from '../interfaces/IPokemon.interface';
+import { Observable } from 'rxjs';
+import { map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SantanderService {
+
+  baseUrl: string = 'https://pokeapi.co/api/v2/pokemon';
+
+  constructor(private http: HttpClient) { }
+
+  getPokemon(): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}?limit=100&offset=0`)
+      .pipe(
+        map((response: any) => response.results),
+      );
+  } 
+
+  getPokemonById(id: string) : Observable<IPokemon>{
+    return this.http.get<IPokemon>(`${this.baseUrl}/${id}`).pipe(
+      map((response: any) => ({name: response.name, url: response.sprites.other.dream_world.front_default}))
+    )
+  }
+}
